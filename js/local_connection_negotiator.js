28b03c28c956dd29c18ad3a1fae22b2d061c1da0
Hayden Faulds <fauldsh@gmail.com>
2015-01-13 10:01:15 +0000

    fix machima class

--- js/local_connection_negotiator.js
+++ js/local_connection_negotiator.js
@@ -1,91 +1,89 @@
 var _ = require('underscore');
 Machina = require('machina');
 
-module.exports = function() {
-  return new Machina.Fsm({
-    initialState: "waitingForOfferer",
+module.exports = Machina.Fsm.extend({
+  initialState: "waitingForOfferer",
 
-    shareIceCandidate : function(con, candidate) {
-      if(con == this.offererCon) {
-        if(this.answererCon) {
-          this.answererCon.handle("addIceCandidate", candidate, this.offererConId);
-        } else {
-          this.deferUntilTransition('waitingForAnswerCreation');
-        }
-      } else if(con == this.answererCon) {
-        this.offererCon.handle("addIceCandidate", candidate, this.offererConId);
+  shareIceCandidate : function(con, candidate) {
+    if(con == this.offererCon) {
+      if(this.answererCon) {
+        this.answererCon.handle("addIceCandidate", candidate, this.offererConId);
+      } else {
+        this.deferUntilTransition('waitingForAnswerCreation');
+      }
+    } else if(con == this.answererCon) {
+      this.offererCon.handle("addIceCandidate", candidate, this.offererConId);
+    }
+  },
+
+  states : {
+    "waitingForOfferer" : {
+      "connect" : function(con) {
+        this.offererConId = con.handle("createOffer");
+        this.offererCon = con;
+        this.transition("waitingForOffer");
       }
     },
 
-    states : {
-      "waitingForOfferer" : {
-        "connect" : function(con) {
-          this.offererConId = con.handle("createOffer");
-          this.offererCon = con;
-          this.transition("waitingForOffer");
-        }
+    "waitingForOffer" : {
+      "connect" : function(con) {
+        this.deferUntilTransition('waitingForAnswerer');
       },
-
-      "waitingForOffer" : {
-        "connect" : function(con) {
-          this.deferUntilTransition('waitingForAnswerer');
-        },
-        "shareOffer" : function(offer) {
-          this.offer = offer;
-          this.transition("waitingForAnswerer");
-        },
-        "shareIceCandidate" : function(con, candidate) {
-          this.shareIceCandidate(con, candidate);
-        }
+      "shareOffer" : function(offer) {
+        this.offer = offer;
+        this.transition("waitingForAnswerer");
       },
+      "shareIceCandidate" : function(con, candidate) {
+        this.shareIceCandidate(con, candidate);
+      }
+    },
 
-      "waitingForAnswerer" : {
-        "connect" : function(con) {
-          this.answererCon = con;
-          this.answererConId = con.handle("receiveOffer", this.offer);
-          this.transition("waitingForAnswerCreation");
-        },
-        "shareIceCandidate" : function(con, candidate) {
-          this.shareIceCandidate(con, candidate);
-        }
+    "waitingForAnswerer" : {
+      "connect" : function(con) {
+        this.answererCon = con;
+        this.answererConId = con.handle("receiveOffer", this.offer);
+        this.transition("waitingForAnswerCreation");
       },
+      "shareIceCandidate" : function(con, candidate) {
+        this.shareIceCandidate(con, candidate);
+      }
+    },
 
-      "waitingForAnswerCreation" : {
-        "shareAnswer" : function(answer) {
-          this.answer = answer;
-          this.transition("waitingForAnswerAccept");
-        },
-        "shareIceCandidate" : function(con, candidate) {
-          this.shareIceCandidate(con, candidate);
-        }
+    "waitingForAnswerCreation" : {
+      "shareAnswer" : function(answer) {
+        this.answer = answer;
+        this.transition("waitingForAnswerAccept");
       },
+      "shareIceCandidate" : function(con, candidate) {
+        this.shareIceCandidate(con, candidate);
+      }
+    },
 
-      "waitingForAnswerAccept" : {
-        _onEnter: function() {
-          this.offererCon.handle("acceptAnswer", this.answer, this.offererConId);
-        },
-        "acceptAnswer" : function(answer) {
-          this.transition("waitingForIceCandidates");
-        },
-        "shareIceCandidate" : function(con, candidate) {
-          this.shareIceCandidate(con, candidate);
-        }
+    "waitingForAnswerAccept" : {
+      _onEnter: function() {
+        this.offererCon.handle("acceptAnswer", this.answer, this.offererConId);
       },
-
-      "waitingForIceCandidates" : {
-        "connected" : function(answer) {
-          this.transition("connected");
-        },
-        "shareIceCandidate" : function(con, candidate) {
-          this.shareIceCandidate(con, candidate);
-        }
+      "acceptAnswer" : function(answer) {
+        this.transition("waitingForIceCandidates");
       },
+      "shareIceCandidate" : function(con, candidate) {
+        this.shareIceCandidate(con, candidate);
+      }
+    },
 
-      "connected" : {
-        _onEnter: function() {
+    "waitingForIceCandidates" : {
+      "connected" : function(answer) {
+        this.transition("connected");
+      },
+      "shareIceCandidate" : function(con, candidate) {
+        this.shareIceCandidate(con, candidate);
+      }
+    },
 
-        }
+    "connected" : {
+      _onEnter: function() {
+
       }
     }
-  });
-};
+  }
+});
