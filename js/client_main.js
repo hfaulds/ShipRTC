1f0456eea19cfe9e1540e59bcd7d3936322bf82b
Hayden Faulds <fauldsh@gmail.com>
2015-01-30 19:29:05 +0000

    create event coord class

--- js/client_main.js
+++ js/client_main.js
@@ -1,5 +1,6 @@
 var Server = require("./client/server");
 var Client = require("./client/client");
+var EventCoord = require("./utils/event_coord");
 
 var lobbyServerURL = "http://localhost:9999";
 
@@ -8,18 +9,19 @@ var client1 = new Client(lobbyServerURL);
 var client2 = new Client(lobbyServerURL);
 
 server.on("registered", function(lobbyId) {
-  console.log('server registered');
   client1.handle('connectToServer', lobbyId);
   client2.handle('connectToServer', lobbyId);
 });
 
 client1.on("connected", function() {
   client1.handle('sendMessage', 'from client1');
-  server.handle('sendMessage', 'from server');
 });
 
 client2.on("connected", function() {
   client2.handle('sendMessage', 'from client2');
+});
+
+EventCoord(client1, client2).onAll("connected", function() {
   server.handle('sendMessage', 'from server');
 });
 
---
+++ js/utils/event_coord.js
@@ -0,0 +1,44 @@
+var _ = require('underscore');
+
+module.exports = function() {
+  var subjects = Array.prototype.slice.call(arguments);
+  var doneSubjects = [];
+
+  function hasSubjectEmitted(subject) {
+    return !_.contains(doneSubjects, subject);
+  }
+
+  function haveAllSubjectsEmitted() {
+    return _.isEqual(doneSubjects, subjects);
+  }
+
+  function unsubscribeAll() {
+    _.each(subjects, function(subject) {
+      subject.off(event);
+    });
+  }
+
+  function onEvent(subject, callback) {
+    if(hasSubjectEmitted(subject)) {
+      doneSubjects.push(subject);
+      if(haveAllSubjectsEmitted()) {
+        callback();
+        unsubscribeAll();
+      }
+    }
+  }
+
+  function subscribeAll(subjects, event, callback) {
+    _.each(subjects, function(subject) {
+      subject.on(event, function() {
+        onEvent(subject, callback);
+      });
+    });
+  }
+
+  return {
+    onAll: function(event, callback) {
+      subscribeAll(subjects, event, callback);
+    }
+  };
+};
