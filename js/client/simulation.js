4e59aa111406ede99a1c1402ce7d3b54f27e334e
Hayden Faulds <fauldsh@gmail.com>
2015-02-27 19:01:16 +0000

    unlink simulation from tick rate

--- js/client/simulation.js
+++ js/client/simulation.js
@@ -1,21 +1,30 @@
 var _ = require('lodash');
 if(global.document) var PIXI = require('pixi.js');
 
+var RADIANS_PER_MS = 200;
+var UNITS_PER_MS = 0.5;
+
 function Simulation() {
   this.playerPositions = {
     'server' : { x: 0, y:0, rotation: 0 }
   };
   this.playerInputs = { };
+  this.lastTickTime = new Date();
 }
 
 Simulation.prototype.tick = function() {
   var that = this;
+
+  var currentTickTime = new Date();
+  var dt = currentTickTime - this.lastTickTime;
+  this.lastTickTime = currentTickTime;
+
   _.each(this.playerInputs, function(input, id) {
     var position = that.playerPositions[id];
 
     if(input.forward && input.forward !== 0) {
       var movementDirection = new PIXI.Matrix();
-      movementDirection.translate(0, input.forward * 5);
+      movementDirection.translate(0, input.forward * UNITS_PER_MS * dt);
       movementDirection.rotate(position.rotation);
 
       var movement = movementDirection.apply(new PIXI.Point());
@@ -25,7 +34,7 @@ Simulation.prototype.tick = function() {
     }
 
     if(input.rotation && input.rotation !== 0) {
-      position.rotation += input.rotation * Math.PI / 64;
+      position.rotation += input.rotation * dt / RADIANS_PER_MS;
       position.dirty = true;
     }
 
