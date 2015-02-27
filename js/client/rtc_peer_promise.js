8454fd7591a9ca3cbc7b38d042a9b53a0787a13f
Hayden Faulds <fauldsh@gmail.com>
2015-02-27 13:29:32 +0000

    add some public ice servers

--- js/client/rtc_peer_promise.js
+++ js/client/rtc_peer_promise.js
@@ -1,10 +1,34 @@
 var Bluebird = require("bluebird");
 
+var SERVERS =  [
+  {url:'stun:stun01.sipphone.com'},
+  {url:'stun:stun.ekiga.net'},
+  {url:'stun:stun.fwdnet.net'},
+  {url:'stun:stun.ideasip.com'},
+  {url:'stun:stun.iptel.org'},
+  {url:'stun:stun.rixtelecom.se'},
+  {url:'stun:stun.schlund.de'},
+  {url:'stun:stun.l.google.com:19302'},
+  {url:'stun:stun1.l.google.com:19302'},
+  {url:'stun:stun2.l.google.com:19302'},
+  {url:'stun:stun3.l.google.com:19302'},
+  {url:'stun:stun4.l.google.com:19302'},
+  {url:'stun:stunserver.org'},
+  {url:'stun:stun.softjoys.com'},
+  {url:'stun:stun.voiparound.com'},
+  {url:'stun:stun.voipbuster.com'},
+  {url:'stun:stun.voipstunt.com'},
+  {url:'stun:stun.voxgratia.org'},
+  {url:'stun:stun.xten.com'},
+];
+
+var CONFIG = {iceServers: SERVERS};
+
 function RTCPeerPromise() {
   if(global.webkitRTCPeerConnection) {
-    this.connection = new webkitRTCPeerConnection(null);
+    this.connection = new webkitRTCPeerConnection(CONFIG);
   } else if(global.mozRTCPeerConnection) {
-    this.connection = new mozRTCPeerConnection(null);
+    this.connection = new mozRTCPeerConnection(CONFIG);
   }
 }
 
